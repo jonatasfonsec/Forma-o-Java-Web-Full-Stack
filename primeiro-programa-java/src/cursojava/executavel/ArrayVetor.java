18ae130fb8b5cf51631cef1104995e635408634c
JonatasBFonsec <jonatas.fonsec@gmail.com>
2022-08-31 08:30:50 -0300

    13.2 - Criando nosso primeiro Array

---
+++ primeiro-programa-java/src/cursojava/executavel/ArrayVetor.java
@@ -0,0 +1,29 @@
+package cursojava.executavel;
+
+public class ArrayVetor {
+
+	/*Usado para executar código*/
+	public static void main(String[] args) {
+		
+		/*Array pode ser de todos os tipos de dados e objetos tbm*/
+		
+		
+		/*Array sempre deve ter uma quantidade de posições definidas*/
+		/*Sempre deve ser instanciado*/
+		double notas[] = new double[5];
+		
+		
+		/*Atribuir valor nas posições do Array*/
+		notas[0] = 7.8;
+		notas[1] = 8.7;
+		notas[2] = 9.7;
+		notas[3] = 9.9;
+		
+		System.out.println(notas);
+		
+		//double [] notas;
+		
+		
+	}
+	
+}
