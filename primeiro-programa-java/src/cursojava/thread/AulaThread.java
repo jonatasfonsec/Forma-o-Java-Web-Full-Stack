a6476a4c85d0e820c53345b3843cbb7c95099581
JonatasBFonsec <jonatas.fonsec@gmail.com>
2022-09-08 08:16:30 -0300

    14.5 - Thread em objeto e o Runnable

--- primeiro-programa-java/src/cursojava/thread/AulaThread.java
+++ primeiro-programa-java/src/cursojava/thread/AulaThread.java
@@ -5,27 +5,56 @@ import javax.swing.JOptionPane;
 public class AulaThread {
 	public static void main(String[] args) throws InterruptedException {
 		
-		new Thread(){
+		Thread threadEmail = new Thread(thread1);
+		threadEmail.start();
 			
-			public void run() {/*Executa o conteúdo da thread*/
-				/*Código da rotina*/
-				
-				for (int pos = 0; pos < 10;pos++) {
-					try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					/*Quero executar esse envio com um intervalo.*/
-					System.out.println("Executando alguma rotina, por exemplo: envio de e-mail.");
-				}
-			};
-			
-		}.start();/*Liga a thread*/
-		/*Código do sistema continua o fluxo*/
-		/**/
+	
+		//********************/
+		
+		Thread threadNotaFiscal = new Thread(thread2);
+		threadNotaFiscal.start();	
+
+		
 		JOptionPane.showMessageDialog(null, "Sistema executando...");
 		//System.out.println("CHEGOU AO FIM DO CÓDIGO DE TESTE DE THREAD.");
 	}
+	
+	private static Runnable thread2 = new Runnable() {
+		public void run() {
+			for (int pos = 0; pos < 10;pos++) {
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				/*Quero executar esse envio com um intervalo.*/
+				System.out.println("Executando alguma rotina, por exemplo: envio de nota fiscal.");
+			}
+			
+			
+			
+		};
+	};
+	
+	
+	
+	private static Runnable thread1 = new Runnable() {
+		public void run() {
+			/*Código da rotina*/
+			
+			for (int pos = 0; pos < 10;pos++) {
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				/*Quero executar esse envio com um intervalo.*/
+				System.out.println("Executando alguma rotina, por exemplo: envio de e-mail.");
+			}
+			
+		}
+		
+	};
 }
