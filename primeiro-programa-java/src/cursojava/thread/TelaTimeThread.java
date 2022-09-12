728d63a80d6d6cb068dfa066435c7baa9153f8a0
JonatasBFonsec <jonatas.fonsec@gmail.com>
2022-09-12 09:21:47 -0300

    14.8 - Adicionando os botões JButton

--- primeiro-programa-java/src/cursojava/thread/TelaTimeThread.java
+++ primeiro-programa-java/src/cursojava/thread/TelaTimeThread.java
@@ -4,7 +4,9 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -20,7 +22,8 @@ public class TelaTimeThread extends JDialog {
 	private JLabel descricaoHora2 = new JLabel("Time Thread 2");
 	private JTextField mostraTempo2 = new JTextField();
 	
-	
+	private JButton jButton = new JButton ("Start");
+	private JButton jButton2 = new JButton ("Stop");
 	
 
 	public TelaTimeThread() {
@@ -34,6 +37,9 @@ public class TelaTimeThread extends JDialog {
 		GridBagConstraints gridBagConstraints = new GridBagConstraints();
 		gridBagConstraints.gridx = 0;
 		gridBagConstraints.gridy = 0;
+		gridBagConstraints.gridwidth = 2;
+		gridBagConstraints.insets = new Insets(5, 10, 5, 5);
+		gridBagConstraints.anchor = GridBagConstraints.WEST;
 		
 		descricaoHora.setPreferredSize(new Dimension(200,25));
 		jPanel.add(descricaoHora,gridBagConstraints);
@@ -41,6 +47,7 @@ public class TelaTimeThread extends JDialog {
 		
 		mostraTempo.setPreferredSize(new Dimension(200,25));
 		gridBagConstraints.gridy++;
+		mostraTempo.setEditable(false);
 		jPanel.add(mostraTempo,gridBagConstraints);
 		
 		descricaoHora2.setPreferredSize(new Dimension(200, 25));
@@ -49,7 +56,24 @@ public class TelaTimeThread extends JDialog {
 		
 		mostraTempo2.setPreferredSize(new Dimension(200, 25));
 		gridBagConstraints.gridy++;
+		mostraTempo2.setEditable(false);
 		jPanel.add(mostraTempo2,gridBagConstraints);
+		
+		
+		gridBagConstraints.gridwidth=1;
+		
+		
+		
+		
+		jButton.setPreferredSize(new Dimension(92, 25));
+		gridBagConstraints.gridy++;
+		jPanel.add(jButton,gridBagConstraints);
+		
+		jButton2.setPreferredSize(new Dimension(92, 25));
+		gridBagConstraints.gridx++;
+		jPanel.add(jButton2,gridBagConstraints);
+		
+		
 		
 		add(jPanel,BorderLayout.WEST);
 		//Sempre será o último comando
