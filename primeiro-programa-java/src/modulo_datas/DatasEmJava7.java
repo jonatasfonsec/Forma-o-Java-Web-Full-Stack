cd7dc8b7006226d8e0c0516b218a4e4e90c629e5
JonatasBFonsec <jonatas.fonsec@gmail.com>
2022-09-20 09:32:07 -0300

    15.10 - Dia, meses, anos e semanas

---
+++ primeiro-programa-java/src/modulo_datas/DatasEmJava7.java
@@ -0,0 +1,31 @@
+package modulo_datas;
+
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class DatasEmJava7 {
+
+	public static void main(String[] args) throws ParseException {
+		
+		LocalDate localDate = LocalDate.now();
+		System.out.println("Data atual: " + localDate.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+		
+		
+		
+		System.out.println("Dia da semana: " + localDate.getDayOfWeek().ordinal());
+		
+		System.out.println("Dia do Mês: " + localDate.getDayOfMonth());
+		
+		System.out.println("Dia do Ano: " + localDate.getDayOfYear());
+		
+		System.out.println("Mes: " + localDate.getMonth());
+		
+		System.out.println("Ano: " + localDate.getYear());
+		
+		
+		
+
+	}
+
+}
