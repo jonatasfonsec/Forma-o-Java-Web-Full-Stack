d33c314110bbba3f634f6b3327a5ad8002df8adc
JonatasBFonsec <jonatas.fonsec@gmail.com>
2022-09-22 08:21:39 -0300

    16.2 - Lendo as linhas do arquivo de TXT

---
+++ primeiro-programa-java/src/arquivos/LerArquivoTxt.java
@@ -0,0 +1,23 @@
+package arquivos;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class LerArquivoTxt {
+	 public static void main(String[] args) throws FileNotFoundException {
+		
+		 
+		 FileInputStream entradaArquivo = new FileInputStream(new File("C:\\Users\\Jonatas\\git\\repository\\primeiro-programa-java\\src\\arquivos\\arquivo.txt"));
+		 
+		 Scanner lerArquivo = new Scanner(entradaArquivo, "UTF-8");
+		 
+		 while (lerArquivo.hasNext()) {
+			 String linha = lerArquivo.nextLine();
+			 
+			 if (linha != null && !linha.isEmpty())
+			 System.out.println(linha);
+		 }
+	}
+}
