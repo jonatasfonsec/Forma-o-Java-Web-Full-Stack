333bdc8d250ccf8a5c5d447f2846868370bd5d41
JonatasBFonsec <jonatas.fonsec@gmail.com>
2022-09-25 08:33:29 -0300

    16.3 - Lendo arquivo TXT e criando lista de objetos

--- primeiro-programa-java/src/arquivos/LerArquivoTxt.java
+++ primeiro-programa-java/src/arquivos/LerArquivoTxt.java
@@ -3,6 +3,8 @@ package arquivos;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class LerArquivoTxt {
@@ -13,11 +15,25 @@ public class LerArquivoTxt {
 		 
 		 Scanner lerArquivo = new Scanner(entradaArquivo, "UTF-8");
 		 
+		 List<Pessoa> pessoas = new ArrayList<Pessoa>();
+		 
 		 while (lerArquivo.hasNext()) {
 			 String linha = lerArquivo.nextLine();
 			 
-			 if (linha != null && !linha.isEmpty())
-			 System.out.println(linha);
+			 if (linha != null && !linha.isEmpty()) {
+			 //System.out.println(linha);
+				 String[] dados = linha.split("\\;");
+				 
+				 Pessoa pessoa = new Pessoa();
+				 pessoa.setNome(dados[0]);
+				 pessoa.setEmail(dados[1]);
+				 pessoa.setIdade(dados[2]);
+				 
+				 pessoas.add(pessoa);
+				 
+			 }
 		 }
+		 
+		 System.out.println(pessoas);
 	}
 }
